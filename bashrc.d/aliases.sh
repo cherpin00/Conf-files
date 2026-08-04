cf9e689ff1cbfc1c0c05f32c99dc89e5d88c8f37
beccacb3 <beccacb3@gmail.com>
2026-08-03 20:31:17 -0700

    added becca-vm alias
    Signed-off-by: beccacb3 <beccacb3@gmail.com>

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -1,4 +1,5 @@
 alias dir="ls -latrh"
 alias tmux_start="tmux new -A -s tmux_ssh"
+alias becca-vm="ssh beccacb3@192.168.98.202"
 alias tp="trash"
 alias dev='x2ssh -et cherpin.sb.facebook.com -x -c '\''tmux new -A -s tmux_ssh; exit'\'
