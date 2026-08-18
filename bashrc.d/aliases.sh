9e02a445eeff83bb136a821cd7c0ac81352175e6
beccacb3 <beccacb3@gmail.com>
2026-08-18 13:41:39 -0700

    update becca-vm alias

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -1,5 +1,5 @@
 alias dir="ls -latrh"
 alias tmux_start="tmux new -A -s tmux_ssh"
-alias becca-vm="ssh beccacb3@192.168.98.202"
+alias becca-vm="ssh -i ~/.ssh/becca-vm beccacb3@192.168.98.202"
 alias tp="trash"
 alias dev='x2ssh -et cherpin.sb.facebook.com -x -c '\''tmux new -A -s tmux_ssh; exit'\'
